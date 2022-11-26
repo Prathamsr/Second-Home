a0663f8441c6477d8bd4b25f109f8be82efb9595
Pratham <prathamsr29@gmail.com>
2022-11-26 08:22:31 +0530

    grpfinal1

--- try.py
+++
@@ -1,6 +0,0 @@
-import json
-with open('static\javascript/insname.json','w') as f:
-    json.dump(['Pratham','Neelam','Muskan','Jodhpur','jaipur','bikaner','delhi','indore'],f)
-with open('static\javascript/insname.json','r') as f:
-    l=json.load(f)
-l.append('love')
